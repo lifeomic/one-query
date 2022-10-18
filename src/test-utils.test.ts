7e50b2aa956559f0ed1184bc6aa7ed8e34ca8b02
Swain Molster <swain.molster@lifeomic.com>
2022-10-18 14:28:00 -0400

    chore: add msw-powered test-util

---
+++ src/test-utils.test.ts
@@ -0,0 +1,101 @@
+import axios from 'axios';
+import { v4 } from 'uuid';
+import { createAPIMockingUtility } from './test-utils';
+import { APIClient } from './util';
+
+describe('createAPIMockingUtility', () => {
+  type TestEndpoints = {
+    'GET /items': {
+      Request: { filter: string };
+      Response: { message: string };
+    };
+    'GET /items/:id': {
+      Request: { filter: string };
+      Response: { message: string };
+    };
+    'POST /items': {
+      Request: { message: string };
+      Response: { message: string };
+    };
+  };
+
+  const useNetworkMocking = createAPIMockingUtility<TestEndpoints>({
+    baseUrl: 'https://www.lifeomic.com',
+  });
+
+  const network = useNetworkMocking();
+
+  const client = new APIClient<TestEndpoints>(
+    axios.create({ baseURL: 'https://www.lifeomic.com' }),
+  );
+
+  describe('mock(...)', () => {
+    test('GET success with static responses', async () => {
+      const response = { message: v4() };
+      network.mock('GET /items', { status: 200, data: response });
+
+      const result = await client.request('GET /items', { filter: '' });
+
+      expect(result.status).toStrictEqual(200);
+      expect(result.data).toStrictEqual(response);
+    });
+
+    test('GET success with function responses', async () => {
+      network.mock('GET /items/:id', (req) => ({
+        status: 200,
+        data: { message: `${req.query.filter}|${req.params.id}` },
+      }));
+
+      const payload = { id: v4(), filter: v4() };
+      const result = await client.request('GET /items/:id', payload);
+
+      expect(result.status).toStrictEqual(200);
+      expect(result.data).toStrictEqual({
+        message: `${payload.filter}|${payload.id}`,
+      });
+    });
+
+    test('POST success with static responses', async () => {
+      const response = { message: v4() };
+      network.mock('POST /items', { status: 200, data: response });
+
+      const result = await client.request('POST /items', { message: '' });
+
+      expect(result.status).toStrictEqual(200);
+      expect(result.data).toStrictEqual(response);
+    });
+
+    test('POST success with function responses', async () => {
+      network.mock('POST /items', (req) => ({
+        status: 200,
+        data: { message: req.body.message },
+      }));
+
+      const payload = { message: v4() };
+      const result = await client.request('POST /items', payload);
+
+      expect(result.status).toStrictEqual(200);
+      expect(result.data).toStrictEqual({ message: payload.message });
+    });
+  });
+
+  test('mockOrdered', async () => {
+    network.mockOrdered('GET /items', [
+      { status: 200, data: { message: '1' } },
+      { status: 200, data: { message: '2' } },
+      { status: 200, data: { message: '3' } },
+    ]);
+
+    const first = await client.request('GET /items', { filter: '' });
+    expect(first.status).toStrictEqual(200);
+    expect(first.data).toStrictEqual({ message: '1' });
+
+    const second = await client.request('GET /items', { filter: '' });
+    expect(second.status).toStrictEqual(200);
+    expect(second.data).toStrictEqual({ message: '2' });
+
+    const third = await client.request('GET /items', { filter: '' });
+    expect(third.status).toStrictEqual(200);
+    expect(third.data).toStrictEqual({ message: '3' });
+  });
+});
---
+++ src/test-utils.ts
@@ -0,0 +1,206 @@
+import * as msw from 'msw';
+import { setupServer, SetupServerApi } from 'msw/node';
+import { PathParamsOf, RoughEndpoints } from './types';
+
+export type APIMockerResponse<T> =
+  | { status: 200; data: T }
+  | { status: 400 | 401 | 403 | 404 | 500; data: any };
+
+type MockRequestHandler<
+  Endpoints extends RoughEndpoints,
+  Route extends keyof Endpoints,
+> = (
+  // prettier-ignore
+  request:
+  & {
+      headers: Record<string, string | undefined>;
+      params: PathParamsOf<Route>;
+    }
+  & (Route extends (`GET ${string}` | `DELETE ${string}`)
+    ? { query: Endpoints[Route]['Request'] }
+    : { body: Endpoints[Route]['Request'] }),
+) =>
+  | APIMockerResponse<Endpoints[Route]['Response']>
+  | Promise<APIMockerResponse<Endpoints[Route]['Response']>>;
+
+type MockFunction<Endpoints extends RoughEndpoints> = <
+  Route extends keyof Endpoints & string,
+>(
+  route: Route,
+  handlerOrResponse:
+    | APIMockerResponse<Endpoints[Route]['Response']>
+    | MockRequestHandler<Endpoints, Route>,
+) => APIMocker<Endpoints>;
+
+export type APIMocker<Endpoints extends RoughEndpoints> = {
+  /**
+   * Persistently mocks the specified `route` using the provided handler or
+   * static response.
+   */
+  mock<Route extends keyof Endpoints & string>(
+    route: Route,
+    handlerOrResponse:
+      | APIMockerResponse<Endpoints[Route]['Response']>
+      | MockRequestHandler<Endpoints, Route>,
+  ): APIMocker<Endpoints>;
+
+  /**
+   * Mocks a series of ordered responses on the specified `route`. Each of
+   * the responses will be returned exactly once, in the order they were
+   * provided.
+   *
+   * @example
+   *
+   * api.mockOrdered(
+   *   'GET /something',
+   *   [
+   *     { status: 200, data: { value: 'one' } },
+   *     { status: 200, data: { value: 'two' } },
+   *     { status: 200, data: { value: 'three' } },
+   *   ]
+   * );
+   *
+   * const res1 = await client.get('/something'); // { value: 'one' }
+   * const res2 = await client.get('/something'); // { value: 'two' }
+   * const res2 = await client.get('/something'); // { value: 'three' }
+   */
+  mockOrdered<Route extends keyof Endpoints & string>(
+    route: Route,
+    handlerOrResponse: (
+      | APIMockerResponse<Endpoints[Route]['Response']>
+      | MockRequestHandler<Endpoints, Route>
+    )[],
+  ): APIMocker<Endpoints>;
+
+  reset: () => void;
+};
+
+export const createAPIMocker = <Endpoints extends RoughEndpoints>(
+  server: SetupServerApi,
+  baseUrl: string,
+): APIMocker<Endpoints> => {
+  const api: APIMocker<Endpoints> = {} as any;
+
+  const createMocker =
+    (options: { once: boolean }): MockFunction<Endpoints> =>
+    (route, handlerOrResponse) => {
+      const [method, url] = route.split(' ');
+
+      const lowercaseMethod = method.toLowerCase() as
+        | 'get'
+        | 'delete'
+        | 'put'
+        | 'patch'
+        | 'post';
+
+      server.use(
+        msw.rest[lowercaseMethod](`${baseUrl}${url}`, async (req, res, ctx) => {
+          const resolve = options.once ? res.once : res;
+
+          if (typeof handlerOrResponse !== 'function') {
+            return resolve(
+              ctx.status(handlerOrResponse.status),
+              ctx.json(handlerOrResponse.data),
+            );
+          }
+
+          const mockRequest = {
+            headers: req.headers.all(),
+            params: req.params as PathParamsOf<typeof route>,
+          };
+
+          let mockedResponse: APIMockerResponse<
+            Endpoints[typeof route]['Response']
+          >;
+
+          if (['get', 'delete'].includes(lowercaseMethod)) {
+            const query: Record<string, string> = {};
+            for (const [key, value] of req.url.searchParams.entries()) {
+              query[key] = value;
+            }
+            // @ts-expect-error TypeScript isn't smart enough to narrow down
+            // the GET/DELETE case here.
+            mockedResponse = await handlerOrResponse({
+              ...mockRequest,
+              query,
+            });
+          } else {
+            const body = await req.json();
+            // @ts-expect-error TypeScript isn't smart enough to narrow down
+            // the GET/DELETE case here.
+            mockedResponse = await handlerOrResponse({
+              ...mockRequest,
+              body,
+            });
+          }
+
+          return resolve(
+            ctx.status(mockedResponse.status),
+            ctx.json(mockedResponse.data),
+          );
+        }),
+      );
+
+      return api;
+    };
+
+  api.mock = createMocker({ once: false });
+
+  const mockOnce = createMocker({ once: true });
+  api.mockOrdered = (route, handlerOrResponses) => {
+    // msw's behavior is the opposite of what we want here: they return _later_
+    // mocks first. So, reverse this array to get the behavior we want.
+    for (const handlerOrResponse of handlerOrResponses.concat().reverse()) {
+      mockOnce(route, handlerOrResponse);
+    }
+    return api;
+  };
+
+  api.reset = () => {
+    server.resetHandlers();
+  };
+
+  return api;
+};
+
+export type CreateAPIMockingConfig = {
+  baseUrl: string;
+};
+
+/**
+ * Creates a type-friendly reusable "hook" for mocking API requests.
+ *
+ * @example
+ * type MyEndpoints = {
+ *   'GET /something': {
+ *     Request: { filter: string }
+ *     Response: { message: string }
+ *   }
+ * }
+ *
+ * // test-utils.ts
+ * export const useAPIMocking = createAPIMockingUtility<MyEndpoints>({ baseUrl: '...' })
+ *
+ * // network.test.ts
+ * const api = useAPIMocking();
+ *
+ * api.mock('GET /something', { status: 200, data: { message: 'test-message' } })
+ */
+export const createAPIMockingUtility =
+  <Endpoints extends RoughEndpoints>({ baseUrl }: CreateAPIMockingConfig) =>
+  () => {
+    const server = setupServer();
+    server.listen({ onUnhandledRequest: 'error' });
+
+    const mocker = createAPIMocker<Endpoints>(server, baseUrl);
+
+    beforeEach(() => {
+      mocker.reset();
+    });
+
+    afterAll(() => {
+      server.close();
+    });
+
+    return mocker;
+  };
